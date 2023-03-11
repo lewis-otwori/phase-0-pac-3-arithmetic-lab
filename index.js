a268008c64d9f26486758a06abd857877ec48bd8
lewis-otwori <lewisotwori@gmail.com>
2023-03-11 08:34:08 +0300

    Completed assingments

---
+++ index.js
@@ -0,0 +1,61 @@
+function add() {
+    // add function code here
+  }
+  
+  function subtract() {
+    // subtract function code here
+  }
+  
+  function multiply() {
+    // multiply function code here
+  }
+  
+  function divide() {
+    // divide function code here
+  }
+  
+  function add(a, b) {
+    // add function code here
+    return a+b
+  }
+  a+b(2, 5)
+  function subtract(a, b) {
+    // subtract function code here
+    return a-b
+  }
+  a-b(10, 5)
+  
+  function multiply(a, b) {
+    // multiply function code here
+    return a*b
+  }
+  a*b(2, 2)
+  
+  function divide(a, b) {
+    // divide function code here
+    return a/b
+  }
+  a/b(10, 2)
+
+  function increment(n) {
+    return n + 1;
+  }
+  console.log(increment(5)); 
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  console.log(decrement(5)); 
+
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  const n = "10";
+  const parsedInt = makeInt(n);
+  console.log(parsedInt);
+  
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
+    
